a2a7ae05492f0544baf386a06ead0364174d8646
mtso <mtso@tuta.io>
2017-01-04 16:09:23 -0800

    Finish count.In func

--- count.go
+++ count.go
@@ -1,9 +1,88 @@
 package syllables
 
 import (
-
+	"strings"
+	// "log"
 )
 
+type counter struct {
+	count, index, length int
+	singular             string
+	parts                []string
+}
+
 func In(text string) int {
-	return -1
+
+	// Prepare input text by converting to lowercase
+	// and removing all non-alphabetic runes
+	text = strings.ToLower(text)
+	text = expressionNonalphabetic.ReplaceAllString(text, "")
+
+	// Return early when possible
+	if len(text) < 1 {
+		return 0
+	}
+	if len(text) < 3 {
+		return 1
+	}
+
+	// If value is part of cornercases,
+	// return hardcoded value
+	if syllables, ok := cornercases[text]; ok {
+		return syllables
+	}
+
+	// Initialize counter
+	c := counter{}
+
+	// Count and remove matched prefixes and suffixes
+	text = expressionTriple.ReplaceAllStringFunc(text, c.countAndRemove(3))
+	text = expressionDouble.ReplaceAllStringFunc(text, c.countAndRemove(2))
+	text = expressionSingle.ReplaceAllStringFunc(text, c.countAndRemove(1))
+
+	// Count multiple consanants
+	c.parts = consanants.Split(text, -1)
+	c.index = 0
+	c.length = len(c.parts)
+
+	for ; c.index < c.length; c.index++ {
+		if c.parts[c.index] != "" {
+			c.count++
+		}
+	}
+
+	// Subtract one for maches which should be
+	// counted as one but are counted as two
+	subtractOne := c.countInPlace(-1)
+	expressionMonosyllabicOne.ReplaceAllStringFunc(text, subtractOne)
+	expressionMonosyllabicTwo.ReplaceAllStringFunc(text, subtractOne)
+
+	// Add one for maches which should be
+	// counted as two but are counted as one
+	addOne := c.countInPlace(1)
+	expressionDoubleSyllabicOne.ReplaceAllStringFunc(text, addOne)
+	expressionDoubleSyllabicTwo.ReplaceAllStringFunc(text, addOne)
+	expressionDoubleSyllabicThree.ReplaceAllStringFunc(text, addOne)
+	expressionDoubleSyllabicFour.ReplaceAllStringFunc(text, addOne)
+
+	// log.Println(text)
+
+	if c.count < 1 {
+		return 1
+	}
+	return c.count
+}
+
+func (c *counter) countAndRemove(increment int) func(string) string {
+	return func(in string) string {
+		c.count += increment
+		return ""
+	}
+}
+
+func (c *counter) countInPlace(increment int) func(string) string {
+	return func(in string) string {
+		c.count += increment
+		return in
+	}
 }
--- count_test.go
+++ count_test.go
@@ -4,10 +4,10 @@ import (
 	"testing"
 )
 
-func Test_CountIn (t *testing.T) {
+func Test_CountIn(t *testing.T) {
 	cases := []struct {
 		want int
-		in string
+		in   string
 	}{
 		// Original test cases
 		{3, "syllable"},
@@ -17,10 +17,19 @@ func Test_CountIn (t *testing.T) {
 		{1, "mmmmmmmmmmmmmmmm"},
 		{2, "hoopty"},
 
-		{6, "syllables in this phrase"},		
-		{31, "Go is an open source programming language that makes it easy to build simple, reliable, and efficient software."},		
-		{102, "If you have just untarred a binary Go distribution, you need to set the environment variable $GOROOT to the full path of the go directory (the one containing this file). You can omit the variable if you unpack it into /usr/local/go, or if you rebuild from sources by running all.bash (see doc/install-source.html). You should also add the Go binary directory $GOROOT/bin to your shell's path."},		
-		{2, "template"},		
+		// Additional test cases
+		{6, "syllables in this phrase"},
+		{31, "Go is an open source programming language that makes it easy to build simple, reliable, and efficient software."},
+		{102, "If you have just untarred a binary Go distribution, you need to set the environment variable $GOROOT to the full path of the go directory (the one containing this file). You can omit the variable if you unpack it into /usr/local/go, or if you rebuild from sources by running all.bash (see doc/install-source.html). You should also add the Go binary directory $GOROOT/bin to your shell's path."},
+		{2, "template"},
+		{4, "abalone"},
+		{5, "aborigine"},
+		{3, "simile"},
+		{4, "facsimile"},
+		{3, "syncope"},
+		{0, ""},
+		{1, "yo"},
+		{3, "kilogram"},
 	}
 
 	for _, c := range cases {
@@ -30,4 +39,3 @@ func Test_CountIn (t *testing.T) {
 		}
 	}
 }
-
